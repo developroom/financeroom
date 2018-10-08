e8394e6b7c96fd195c48b72aef09c1a463660745
Kamil Musiał <kmusial40@gmail.com>
2018-10-08 22:06:48 +0200

    section offer not responsive done

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 //function that is main engine
 document.addEventListener('DOMContentLoaded',()=>{
     menuOperation();
+    offerOperation();
 });
 
 //function that operate menu
@@ -12,4 +13,17 @@ const menuOperation =()=>{
         barAnimation.classList.toggle('active');
         navAnimation.classList.toggle('active');
     });
+};
+
+//function that operate toggle section
+const offerOperation =()=>{
+    let sectionToggle = document.querySelectorAll('section.offerSection .offerCardHolder .offerCardHolderInner');
+    let headingToggle = document.querySelectorAll('section.offerSection .offerCardHolder .offerCardHolderInner h3.offerCardHolderInnerHeading');
+    let paragrafToggle = document.querySelectorAll('section.offerSection .offerCardHolder .offerCardHolderInner p.offerCardHolderInnerParagraf');
+    for(let i = 0;i< sectionToggle.length;i++){
+        sectionToggle[i].addEventListener('click',()=>{
+            headingToggle[i].classList.toggle('disable');
+            paragrafToggle[i].classList.toggle('active');
+        });
+    }
 };
