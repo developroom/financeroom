11d3e40709aeab4e4b87043e23f4b102d530df66
Kamil Musiał <kmusial40@gmail.com>
2018-10-30 21:05:59 +0100

    some chages of mechanickes sss

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
 var on = 0;
 
 //function that is main engine
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     menuOperation();
     offerOperation();
     //factsSection();
@@ -11,32 +11,36 @@ document.addEventListener('DOMContentLoaded',()=>{
 });
 
 //function that operate menu
-const menuOperation =()=>{
+const menuOperation = () => {
     let buttonMenu = document.querySelector('button.navHamburgerButton')
-    buttonMenu.addEventListener('click',()=>{
+    buttonMenu.addEventListener('click', () => {
         let barAnimation = document.querySelector('button.navHamburgerButton .navHamburgerButtonBarMiddle');
         let navAnimation = document.querySelector('nav.mainNav ul.navList');
         let navToggle = document.querySelector('nav.mainNav');
         barAnimation.classList.toggle('active');
         navAnimation.classList.toggle('active');
         navToggle.classList.toggle('active');
-        let buttons = document.querySelectorAll('.navListItemLink');
-        for(let i =0; i < buttons.length; i++){
-            buttons[i].addEventListener('click',()=>{
-                barAnimation.classList.toggle('active');
-                navAnimation.classList.toggle('active');
-                navToggle.classList.toggle('active');
-            });
-        }
+
     });
+    let buttons = document.querySelectorAll('.navListItemLink');
+    for (let i = 0; i < buttons.length; i++) {
+        let barAnimation = document.querySelector('button.navHamburgerButton .navHamburgerButtonBarMiddle');
+        let navAnimation = document.querySelector('nav.mainNav ul.navList');
+        let navToggle = document.querySelector('nav.mainNav');
+        buttons[i].addEventListener('click', () => {
+            barAnimation.classList.toggle('active');
+            navAnimation.classList.toggle('active');
+            navToggle.classList.toggle('active');
+        });
+    }
 };
 
 //function that operate toggle section
-const offerOperation =()=>{
+const offerOperation = () => {
     let sectionToggle = document.querySelectorAll('section.offerSection .offerCardHolder .offerCardHolderInner');
     let paragrafToggle = document.querySelectorAll('section.offerSection .offerCardHolder .offerCardHolderInner p.offerCardHolderInnerParagraf');
-    for(let i = 0;i< sectionToggle.length;i++){
-        sectionToggle[i].addEventListener('click',()=>{
+    for (let i = 0; i < sectionToggle.length; i++) {
+        sectionToggle[i].addEventListener('click', () => {
             paragrafToggle[i].classList.toggle('active');
             sectionToggle[i].classList.toggle('disable');
             sectionToggle[i].classList.toggle('off');
@@ -45,20 +49,20 @@ const offerOperation =()=>{
 };
 
 //function that destroy cookie footer
-const cookieOff = ()=>{
+const cookieOff = () => {
     let button = document.querySelector('footer.mainFooter button.cookieClose');
-    button.addEventListener('click',()=>{
+    button.addEventListener('click', () => {
         let footer = document.querySelector('footer.mainFooter');
         document.body.removeChild(footer);
     });
 };
 
 //fuction of rodo aceptation
-const rodoAcept = (on)=>{
-    if(on == 0){
+const rodoAcept = (on) => {
+    if (on == 0) {
         let forms = document.querySelectorAll('.formInput');
-        for(let i = 0;i < forms.length; i++){
-            forms[i].addEventListener('click',()=>{
+        for (let i = 0; i < forms.length; i++) {
+            forms[i].addEventListener('click', () => {
                 alert(`ASRoom  z siedzibą w Krzyszkowicach, adres 32-455 Krzyszkowice 590  jako administrator Państwa danych osobowych, mając na względzie dostosowanie się do nowych obowiązków w zakresie przetwarzania danych osobowych w szczególności wynikających z przepisów rozporządzenia Parlamentu Europejskiego i Rady ( UE) 2016/679 z 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE ( RODO ) przekazuje informacje dotyczące przetwarzania przez nas Państwa danych osobowych.
                 Zgodnie z art. 13. Ust. 1 i ust 2 RODO, Regis Sp. z o.o. informuje, że:
                 1.Administratorem pozostawionych danych osobowych jest ASRoom  z siedzibą w Krzyszkowicach, adres 32-455 Krzyszkowice 590  NIP 6831877884 . Kontakt do administratora : asala@financeroom.pl
